d2507868ae59bfb102b3f9d13aab55334289b32b
steadfast-motherboard-7317 <imadejr@yahoo.com>
2019-10-01 22:34:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
  def reverse_each_word(string)
    
    letters = string.split("")
+   reversed = []
    
-    letters.each do |ele| 
-    
+   letters.each do |ele| 
+    reversed.unshift(ele)
    end 
    
  end 
