60c7b0568d598a6802e59b917ead1529ece60863
steadfast-motherboard-7317 <imadejr@yahoo.com>
2019-10-01 22:46:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
  def reverse_each_word(string)
    
    words = string.split(" ")
-   reversed = []
    
-   words.each do |word| 
-      reversed.push(word.reverse)
+   words.collect{ |word| word.reverse}
    end 
    
    reversed.join(" ")
